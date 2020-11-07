ce22cbe4a69cb80b7b8e33c6cd1a064380c5b428
Niki Joel <niki_ponce@hotmail.com>
2020-11-07 16:34:48 +0100

    bash script

--- README.md
+++ README.md
@@ -1,30 +1,42 @@
 # vue-php-fpm-nginx
 
 etc/hosts
-
 127.0.0.1       portainer.testy.com
 127.0.0.1       phpmyadmin.testy.com
 127.0.0.1       api.testy.com
 127.0.0.1       testy.com
 
 
+- ./start.sh all -> start all
+- ./start.sh stop -> stop all
+- ./start.sh base -> start nginx|bbdd|portainer
+- ./start.sh start -> start api|front
+- ./start.sh api -> start api
+- ./start.sh front -> start front
+
+
+
+## Nginx --
+testy.com
+cd nginx_proxy && docker-compose up -b -d
+
 ## vue --
 testy.com
-cd front && docker-compose up --build
+cd front && docker-compose up -b -d
 
 new vue project
-command: /bin/sh -c "yarn global add @vue/cli && vue create . -y"
+command: /bin/sh -c "yarn global add @vue/cli && vue create . "
 
 exist project
 command: /bin/sh -c "yarn && yarn serve"
 
 ## api Symfony --
 api.testy.com
-cd api && docker-compose up --build
+cd api && docker-compose up -b -d
 
 ## bbdd --
 phpmyadmin.testy.com
-cd bbdd && docker-compose up --build
+cd bbdd && docker-compose up -b -d
 
 ## portainer
 portainer.testy.com
--- start.sh
+++ start.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
-for i in {1..10}
-    do cd nginx_proxy && docker-compose up --build
-    sleep 3
-done
+case "$1" in
+        "all")
+            echo "all"
+            cd ./nginx_proxy; docker-compose up --build -d; echo 'start proxy'
+            cd ../bbdd; docker-compose up --build -d; echo 'start bbdd'
+            cd ../api; docker-compose up --build -d; echo 'start api'
+            cd ../front; docker-compose up --build -d; echo 'start front'
+            cd ../portainer; docker-compose up --build -d; echo 'start portainer'
+            ;;
+
+        "stop")
+            echo "stop"
+            cd ./nginx_proxy; docker-compose down; echo 'start proxy'
+            cd ../bbdd; docker-compose down; echo 'start bbdd'
+            cd ../api; docker-compose down; echo 'start api'
+            cd ../front; docker-compose down; echo 'start front'
+            cd ../portainer; docker-compose down; echo 'start portainer'
+            ;;
+
+        "base")
+            echo "base"
+            cd ./nginx_proxy; docker-compose up --build -d; echo 'start proxy'
+            cd ../bbdd; docker-compose up --build -d; echo 'start bbdd'
+            cd ../portainer; docker-compose up --build -d; echo 'start portainer'
+            ;;
+
+        "start")
+            echo "start"
+            cd ../api; docker-compose up --build -d; echo 'start api'
+            cd ../front; docker-compose up --build -d; echo 'start front'
+            ;;
+
+        "api")
+            echo "api"
+            cd ../api; docker-compose up --build -d; echo 'start api'
+            cd ../front; docker-compose up --build -d; echo 'start front'
+            ;;
+
+        "front")
+            echo "front"
+            cd ../api; docker-compose up --build -d; echo 'start api'
+            cd ../front; docker-compose up --build -d; echo 'start front'
+            ;;
+        *)
+            echo $"Usage: $0 {all|stop|base|start|api|front}"
+            exit 1
+esac
